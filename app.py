1e9aeaf41893669dc558c3a3199d3d93d18aa0a7
CarpioNick4 <carpio.nick@iisgalvanimi.edu.it>
2025-02-10 11:46:36 +0000

    finito pt2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "f8c2cc355fbe4afcb4eb8073cfbc2266"
 SPOTIFY_CLIENT_SECRET = "d74c18dff1f14efba2ec5ec59384b363"
-SPOTIFY_REDIRECT_URI = "https://5000-carpionick4-spotifyapi-pq47hk8c9ll.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+SPOTIFY_REDIRECT_URI = "https://5000-carpionick4-spotifyapi-zaeuq5lvi49.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
@@ -16,6 +16,14 @@ sp_oauth = SpotifyOAuth(
     client_secret=SPOTIFY_CLIENT_SECRET,
     redirect_uri=SPOTIFY_REDIRECT_URI,
     scope="user-read-private" #permessi x informazioni dell'utente
+)
+#config SpotifyOAuth per l'autenticazione e redirect uri
+sp_oauth = SpotifyOAuth(
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope="user-read-private", #permessi x informazioni dell'utente
+    show_dialog=True #forziamo la richiesta di inserire new credenziali
 )
 
 @app.route('/')
@@ -37,8 +45,15 @@ def home():
         return redirect(url_for('login'))
     sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
     user_info = sp.current_user()
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items'] #prendiamo solo la lista delle playlist
     print(user_info) #capiamo la struttura di user_info per usarle nel frontend
-    return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
+    return render_template('home.html', user_info=user_info, playlists=playlists_info) #passo le info utente all'home.html
+
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
 
 #avvio dell'app Flask
 if __name__ == '__main__':
